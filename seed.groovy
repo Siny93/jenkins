2dcdb36a6701f03c17aa5008a674eee93fa1701f
Siny <sinydaniel93@gmail.com>
2022-04-27 13:29:54 +0530

    seed job with components

--- seed.groovy
+++ seed.groovy
@@ -57,13 +57,13 @@ for(i in 0..SIZE) {
   def j = COMPONENTS[i]
 
 
-pipelineJob('CI-Pipelines/${j}') {
+pipelineJob('CI-Pipelines/j') {
   configure { flowdefinition ->
     flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
       'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
         'userRemoteConfigs' {
           'hudson.plugins.git.UserRemoteConfig' {
-            'url'("https://github.com/Siny93/${j}.git")
+            'url'("https://github.com/Siny93/j.git")
           }
         }
         'branches' {

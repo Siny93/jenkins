b87ff03b930ffb06904f638b75590c10da126950
Siny <sinydaniel93@gmail.com>
2022-10-05 20:51:16 -0400

    seed groovy for terraform-vpc

--- seed.groovy
+++ seed.groovy
@@ -21,14 +21,77 @@
 
 
 
+//
+//pipelineJob('roboshop-ansible') {
+//  configure { flowdefinition ->
+//    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+//      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+//        'userRemoteConfigs' {
+//          'hudson.plugins.git.UserRemoteConfig' {
+//            'url'('https://github.com/Siny93/ANSIBLE1.git')
+//          }
+//        }
+//        'branches' {
+//          'hudson.plugins.git.BranchSpec' {
+//            'name'('*/main')
+//          }
+//        }
+//      }
+//      'scriptPath'('Jenkinsfile')
+//      'lightweight'(true)
+//    }
+//  }
+//}
+//
+//
+//folder('CI-Pipelines') {
+//  displayName('CI-Pipelines')
+//  description('CI-Pipelines')
+//}
+//
+//
+//
+//def COMPONENTS = ["cart", "catalogue", "payment", "shipping", "user", "dispatch"]
+//def SIZE = COMPONENTS.size -1
+//for(i in 0..SIZE) {
+//  def j = COMPONENTS[i]
+//
+//
+//pipelineJob("CI-Pipelines/${j}") {
+//  configure { flowdefinition ->
+//    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+//      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+//        'userRemoteConfigs' {
+//          'hudson.plugins.git.UserRemoteConfig' {
+//            'url'("https://github.com/Siny93/${j}.git")
+//          }
+//        }
+//        'branches' {
+//          'hudson.plugins.git.BranchSpec' {
+//            'name'('*/main')
+//          }
+//        }
+//      }
+//      'scriptPath'('Jenkinsfile')
+//      'lightweight'(true)
+//    }
+//  }
+//}
+//}
 
-pipelineJob('roboshop-ansible') {
+folder('Mutable') {
+  displayName('Mutable')
+  description('Mutable')
+}
+
+
+pipelineJob('Mutable/VPC') {
   configure { flowdefinition ->
     flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
       'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
         'userRemoteConfigs' {
           'hudson.plugins.git.UserRemoteConfig' {
-            'url'('https://github.com/Siny93/ANSIBLE1.git')
+            'url'('https://github.com/Siny93/terraform-vpc.git')
           }
         }
         'branches' {
@@ -41,40 +104,4 @@ pipelineJob('roboshop-ansible') {
       'lightweight'(true)
     }
   }
-}
-
-
-folder('CI-Pipelines') {
-  displayName('CI-Pipelines')
-  description('CI-Pipelines')
-}
-
-
-
-def COMPONENTS = ["cart", "catalogue", "payment", "shipping", "user", "dispatch"]
-def SIZE = COMPONENTS.size -1
-for(i in 0..SIZE) {
-  def j = COMPONENTS[i]
-
-
-pipelineJob("CI-Pipelines/${j}") {
-  configure { flowdefinition ->
-    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
-      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
-        'userRemoteConfigs' {
-          'hudson.plugins.git.UserRemoteConfig' {
-            'url'("https://github.com/Siny93/${j}.git")
-          }
-        }
-        'branches' {
-          'hudson.plugins.git.BranchSpec' {
-            'name'('*/main')
-          }
-        }
-      }
-      'scriptPath'('Jenkinsfile')
-      'lightweight'(true)
-    }
-  }
-}
 }
---
+++ vars/terraform.groovy
@@ -0,0 +1,54 @@
+def call() {
+
+
+    pipeline {
+        agent {
+            label "${BUILD_LABEL}"
+        }
+
+
+
+        environment {}
+
+        parameters {
+
+
+            choice(name: 'Env', choices: ['', 'dev', 'prod'], description: 'Choose Env')
+            choice(name: 'Action', choices: ['', 'apply', 'destroy'], description: 'Choose Action')
+
+
+        }
+
+        stages {
+
+            stage('label builds') {
+                steps {
+                    script {
+
+                        addShortText background: 'white', borderColor: 'white', color: 'red', link: '', text: "${Env}"
+                        addShortText background: 'white', borderColor: 'white', color: 'red', link: '', text: "${Action}"
+                    }
+                }
+            }
+
+
+            stage('Apply terraform action') {
+                steps {
+                    sh '''
+                      terraform init -backend-config=env/${Env}-backend.tfvars
+                      terraform ${Action} -auto-approve=env/${Env}.tfvars
+                    '''
+                }
+            }
+
+
+
+        post {
+            always {
+                cleanWs()
+            }
+
+        }
+
+    }
+}

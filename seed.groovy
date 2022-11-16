e1dcca61d39c0d95abe40a3540139deacb4c2383
Siny <sinydaniel93@gmail.com>
2022-11-15 20:46:00 -0500

    seed job

--- seed.groovy
+++ seed.groovy
@@ -125,3 +125,25 @@ pipelineJob('Mutable/APP-SETUP') {
     }
   }
 }
+
+
+pipelineJob('Mutable/ALB') {
+  configure { flowdefinition ->
+    flowdefinition << delegate.'definition'(class:'org.jenkinsci.plugins.workflow.cps.CpsScmFlowDefinition',plugin:'workflow-cps') {
+      'scm'(class:'hudson.plugins.git.GitSCM',plugin:'git') {
+        'userRemoteConfigs' {
+          'hudson.plugins.git.UserRemoteConfig' {
+            'url'('https://github.com/Siny93/terraform-mutable-alb.git')
+          }
+        }
+        'branches' {
+          'hudson.plugins.git.BranchSpec' {
+            'name'('*/main')
+          }
+        }
+      }
+      'scriptPath'('Jenkinsfile')
+      'lightweight'(true)
+    }
+  }
+}
